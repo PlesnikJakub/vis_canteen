30e72d82db34242d9c903306217cd3a0f56fdd4a
Jakub Plesnik <jakub.plesnik@gmail.com>
2022-11-21 17:11:43 +0100

    add todo

--- Canteen.Application/TransactionScript/CreateMenuForNextWeek.cs
+++ Canteen.Application/TransactionScript/CreateMenuForNextWeek.cs
@@ -1,7 +1,22 @@
-﻿namespace Canteen.Application.TransactionScript
+﻿using Canteen.DataAccess;
+
+namespace Canteen.Application.TransactionScript
 {
     public class CreateMenuForNextWeek
     {
+        private readonly MenuItemTDG _menuItemTDG;
+        public CreateMenuForNextWeek()
+        {
+            _menuItemTDG = new MenuItemTDG();
+        }
 
+        public void Execute()
+        {
+            // TODO get all items and generate menu
+            // there must be one item cheaper than 100
+            // there must be atleas one vegan item per day but no more than 2
+            // generate menu for 5 day 4 meals per day
+            // dont repeat food in a week
+        }
     }
 }
